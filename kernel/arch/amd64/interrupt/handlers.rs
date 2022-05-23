8116a76043a95b73e9c3e5d8bc35c0dda0390a30
beersonthewall <matthew@beersonthewall.com>
2022-05-23 09:33:02 -0500

    Kernel: read information from exception stack frames.
    Add handler macro & ExceptionStackFrame struct. The handler macro pushes rsp into rdi so ISR's get an immutable reference to the stack frame & can print nice error messages.

--- kernel/arch/amd64/interrupt/handlers.rs
+++ kernel/arch/amd64/interrupt/handlers.rs
@@ -1,12 +1,35 @@
 use core::arch::asm;
 
-pub extern "C" fn divide_by_zero_handler() -> ! {
-    log!("EXCEPTION: divide by zero");
+#[derive(Debug)]
+#[repr(C)]
+pub struct ExceptionStackFrame {
+    intruction_ptr: u64,
+    code_segment: u64,
+    cpu_flags: u64,
+    stack_pointer: u64,
+    stack_segment: u64,
+}
+
+pub extern "C" fn divide_by_zero_handler(stack_frame: &ExceptionStackFrame) -> ! {
+    log!("EXCEPTION: divide by zero\n{:?}", stack_frame);
     loop {}
 }
 
 pub extern "C" fn breakpoint_handler() {
     log!("EXCEPTION: breakpoint");
+}
+
+macro_rules! handler {
+    ($name:ident) => {{
+        #[naked]
+        extern "C" fn wrapper() -> ! {
+            unsafe {
+                asm!("mov rdi, rsp; sub rsp, 8; call {x}; iretq",
+                     x = sym $name, options(noreturn))
+            }
+        }
+        wrapper
+    }};
 }
 
 macro_rules! isr {
--- kernel/arch/amd64/interrupt/mod.rs
+++ kernel/arch/amd64/interrupt/mod.rs
@@ -12,7 +12,7 @@ const IDT_SIZE: usize = 256;
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
-        idt.set_handler(0, isr!(divide_by_zero_handler));
+        idt.set_handler(0, handler!(divide_by_zero_handler));
         idt.set_handler(3, isr!(breakpoint_handler));
         idt
     };
@@ -20,7 +20,14 @@ lazy_static! {
 
 pub fn init() {
     IDT.load();
-    
+
     unsafe { asm!("int3", options(nomem, nostack)); }
     log!("We made it back :)");
+    divide_by_zero();
+}
+
+fn divide_by_zero() {
+    unsafe {
+        asm!("mov dx, 0; div dx", out("ax") _, out("dx") _);
+    }
 }
